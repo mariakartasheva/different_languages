0d4d7b94fdf76f2a5bb2f3d9e71d2faabd38c621
mighty_maria <m.bezymyannaya.com>
2019-10-19 19:47:50 -0400

    Change error message in the test

--- test_items.py
+++ test_items.py
@@ -7,5 +7,5 @@ def test_add_to_basket_exists(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/."
     browser.get(link)
 # Найти кнопку добавления в корзину на странице.
-    assert browser.find_element_by_css_selector('.btn-add-to-basket'), '"Add to basket" exists on the page.'
+    assert browser.find_element_by_css_selector('.btn-add-to-basket'), '"Add to basket" does not exist on the page.'
     time.sleep(5)
